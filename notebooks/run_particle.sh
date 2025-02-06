8ae6033c3e6832474f9f7add570c8cfeaa6e4825
danliba <danliba@hotmail.com>
2025-02-06 16:24:49 +0100

    ELPHE meeting Day 2

--- notebooks/run_particle.sh
+++ notebooks/run_particle.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=60G
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=base
 #SBATCH --output=particles_output_jan_%j.log
 #SBATCH --error=particles_error_jan_%j.log
@@ -15,9 +15,9 @@ module load singularity/3.11.5
 
 # Define base parameters
 container_path="/gxfs_work/geomar/smomw662/FESOMparcels_first/parcels-container_2024.10.03-921b2b0.sif"  # Path to your container
-notebook_path="/gxfs_work/geomar/smomw662/FESOMparcels_first/notebooks/Particle_run-ver2.ipynb"
-output_notebook="/gxfs_work/geomar/smomw662/FESOMparcels_first/out_notebooks/out_particle_ver_2.ipynb"
-num_particles=1
+notebook_path="/gxfs_work/geomar/smomw662/FESOMparcels_first/notebooks/Particle_runUVW.ipynb"
+output_notebook="/gxfs_work/geomar/smomw662/FESOMparcels_first/out_notebooks/out_particle_UVW_2.ipynb"
+num_particles=10000
 days=5400
 
 # Run the Jupyter notebook using Papermill inside the container
